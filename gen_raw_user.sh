e829fe1b040c16552e96aac5e9853b17614e7a3f
Nicolò Valenza <nico.vale.spazio@gmail.com>
2021-01-13 22:53:56 +0100

    Delete gen_raw_user.sh

--- gen_raw_user.sh
+++
@@ -1,8 +0,0 @@
-POST="https://www.instagram.com/p/CHotD4aMviU"
-SESSION_ID="sessionid=33953216539%3AbFk9uuYAQB0Ijw%3A19"
-NUMBER="200"
-FILE_NAME="user_raw"
-FILE_PATH="/home/eagle/d/myapp/cassinelli/dir-spam-bot"
-FILE_TYPE="json"
-
-instatouch likers $POST --session $SESSION_ID --count $NUMBER --filename $FILE_NAME --filepath $FILE_PATH --filetype $FILE_TYPE
